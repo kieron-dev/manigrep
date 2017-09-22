a0c583ef2468061f271fcbc4080c9143580356de
Kieron Browne <kieron.browne@gmail.com>
2017-09-22 22:45:17 +0100

    Add an empty test

---
+++ manigrep_test.go
@@ -0,0 +1,16 @@
+package main_test
+
+import (
+	//. "github.com/kieron-pivotal/manigrep"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Manigrep", func() {
+	Context("when I start developing", func() {
+		It("should just work", func() {
+			Expect(true).To(BeTrue())
+		})
+	})
+})

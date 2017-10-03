53c3a0957e60a65c58a42b128f5c25ce789f0d0d
Kieron Browne <kbrowne@pivotal.io>
2017-10-03 17:59:28 +0100

    add searchVal and refactor a bit

--- traverser/traverser.go
+++ traverser/traverser.go
@@ -1,57 +1,71 @@
 package traverser
 
-import "fmt"
-
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 type Traverser struct {
 	data          *interface{}
-	keyStack      []string
 	keyMatcher    func(key string) bool
 	valMatcher    func(val string) bool
-	foundCallback func(path string)
+	foundCallback func(path []string)
 }
 
 func New(data *interface{}) (*Traverser, error) {
 	tr := Traverser{}
 	tr.data = data
-	tr.keyStack = []string{}
 	return &tr, nil
 }
 
-func (tr *Traverser) dfs(data *interface{}) {
+func (tr *Traverser) dfs(data *interface{}, keyStack []string) {
 	if list, ok := (*data).([]map[string]interface{}); ok {
-		fmt.Println("it's a list", list)
 		for i, item := range list {
-			tr.keyStack = append(tr.keyStack, fmt.Sprintf("[%d]", i))
+			newStack := append(keyStack, fmt.Sprintf("[%d]", i))
 			var interfaceItem interface{}
 			interfaceItem = item
-			tr.dfs(&interfaceItem)
+			tr.dfs(&interfaceItem, newStack)
 		}
 	} else if mp, ok := (*data).(map[string]interface{}); ok {
-		fmt.Println("it's a map", mp)
 		for key, val := range mp {
+			newStack := append(keyStack, key)
 			if tr.keyMatcher(key) {
-				tr.foundCallback("foo")
+				tr.foundCallback(newStack)
 			}
-			tr.dfs(&val)
+			tr.dfs(&val, newStack)
 		}
 	} else {
-		fmt.Println("it's not a list or map", *data)
+		valStr := fmt.Sprintf("%v", *data)
+		if tr.valMatcher(valStr) {
+			tr.foundCallback(keyStack)
+		}
 	}
+}
+
+func noMatch(_ string) bool {
+	return false
 }
 
 func (tr *Traverser) SearchKey(keySearch string, resChan chan string) {
 	tr.keyMatcher = func(key string) bool {
 		return strings.Contains(key, keySearch)
 	}
-	tr.valMatcher = func(_ string) bool {
-		return false
+	tr.valMatcher = noMatch
+	tr.foundCallback = func(path []string) {
+		resChan <- strings.Join(path, "/")
 	}
-	tr.foundCallback = func(path string) {
-		resChan <- path
+	tr.dfs(tr.data, []string{""})
+	close(resChan)
+}
+
+func (tr *Traverser) SearchVal(valSearch string, resChan chan string) {
+	tr.keyMatcher = noMatch
+	tr.valMatcher = func(val string) bool {
+		return strings.Contains(val, valSearch)
 	}
-	tr.dfs(tr.data)
-	fmt.Println("Closing resChan")
+	tr.foundCallback = func(path []string) {
+		resChan <- strings.Join(path, "/")
+	}
+	tr.dfs(tr.data, []string{""})
 	close(resChan)
 }
--- traverser/traverser_test.go
+++ traverser/traverser_test.go
@@ -1,7 +1,7 @@
 package traverser_test
 
 import (
-	"fmt"
+	"log"
 
 	"github.com/kieron-pivotal/manigrep/traverser"
 	. "github.com/onsi/ginkgo"
@@ -10,21 +10,37 @@ import (
 
 var _ = Describe("Traverser", func() {
 
-	It("can iterate through a top-level list", func() {
-		var data interface{}
+	var data interface{}
+
+	BeforeEach(func() {
 		data = []map[string]interface{}{
 			map[string]interface{}{"alf": 30},
 			map[string]interface{}{"bob": 50},
 		}
+	})
+
+	It("can match a key value in a list", func() {
 		tr, _ := traverser.New(&data)
 		resChan := make(chan string)
-		go tr.SearchKey("", resChan)
+		go tr.SearchKey("lf", resChan)
 		res := []string{}
 		for r := range resChan {
+			log.Println(r)
 			res = append(res, r)
 		}
-		fmt.Println("res len", len(res))
-		Expect(len(res)).ToNot(Equal(0))
+		Expect(len(res)).To(Equal(1))
+	})
+
+	It("can match a value within a list", func() {
+		tr, _ := traverser.New(&data)
+		resChan := make(chan string)
+		go tr.SearchVal("5", resChan)
+		res := []string{}
+		for r := range resChan {
+			log.Println(r)
+			res = append(res, r)
+		}
+		Expect(len(res)).To(Equal(1))
 	})
 
 })
